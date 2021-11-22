af1b635a80ee77a015382268d90ec7d99c9ed0da
John Andrew Abala <johnandrew.abala@deped.gov.ph>
2021-11-22 09:38:00 +0800

    update index & Map

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,50 @@ let currentPopup: any = undefined;
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
 
+
 WA.room.onEnterZone('clock', () => {
     currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
 })
 
 WA.room.onLeaveZone('clock', closePopUp)
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+   }
+}
+
+// CLOCK 01
+
+WA.room.onEnterZone('clock1', () => {
+    currentPopup =  WA.ui.openPopup("clockPopup1","It's " + time,[]);
+})
+
+WA.room.onLeaveZone('clock1', closePopUp)
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+   }
+}
+
+// SMARTYS NOOK
+
+WA.room.onEnterZone('nook', () => {
+    currentPopup =  WA.ui.openPopup("nookPopup","Welcome to Smartys Nook! ",[{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    });
+}]);
+})
+
+//WA.room.onLeaveZone('nook', closePopUp)
 
 function closePopUp(){
     if (currentPopup !== undefined) {
