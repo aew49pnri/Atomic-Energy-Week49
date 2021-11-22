3f717dba73a8f9b534256a6586ad1c87ff0fbf79
John Andrew Abala <johnandrew.abala@deped.gov.ph>
2021-11-22 10:21:33 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 bootstrapExtra().catch(e => console.error(e));
 
 let currentPopup: any = undefined;
+
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
 
@@ -23,42 +24,4 @@ function closePopUp(){
         currentPopup.close();
         currentPopup = undefined;
    }
-}
-
-// CLOCK 01
-
-WA.room.onEnterZone('clock1', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup1","It's " + time,[]);
-})
-
-WA.room.onLeaveZone('clock1', closePopUp)
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-   }
-}
-
-// SMARTYS NOOK
-
-WA.room.onEnterZone('nook', () => {
-    currentPopup =  WA.ui.openPopup("nookPopup","Welcome to Smartys Nook! ",[{
-        label: "Close",
-        className: "primary",
-        callback: (popup) => {
-            // Close the popup when the "Close" button is pressed.
-            popup.close();
-        }
-    });
-}]);
-})
-
-//WA.room.onLeaveZone('nook', closePopUp)
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
 }
